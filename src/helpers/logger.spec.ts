efe84a9626f6922a85f189cf327bf1b3c34e1f2a
Murat Ersin <mmuratersin@gmail.com>
2022-04-16 20:18:30 +0300

    feat: add test for logger

---
+++ src/helpers/logger.spec.ts
@@ -0,0 +1,21 @@
+import { describe, test, expect } from 'vitest'
+
+import { consoleDebug, consoleError, consoleLog, consoleWarning } from './logger'
+
+describe('Logger ', () => {
+  test('consoleDebug should be a function', () => {
+    expect(consoleDebug).toBeTypeOf('function')
+  })
+
+  test('consoleError should be a function', () => {
+    expect(consoleError).toBeTypeOf('function')
+  })
+
+  test('consoleLog should be a function', () => {
+    expect(consoleLog).toBeTypeOf('function')
+  })
+
+  test('consoleWarning should be a function', () => {
+    expect(consoleWarning).toBeTypeOf('function')
+  })
+})
