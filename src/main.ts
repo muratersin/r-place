f608dac9100831a70debcac9d8880aeaafdb1573
Murat Ersin <mmuratersin@gmail.com>
2022-04-16 15:02:26 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './assets/style/main.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+const place = document.createElement('canvas')
+// place.height = 100006
+// place.width = 100006
+place.setAttribute('id', 'place')
+app.appendChild(place)
